c60fd415d0ddb09bb7dfdad6941b613b578562c8
Pablo Nathanielle Gomez <gomezdevlopment@gmail.com>
2022-09-22 13:12:16 -0400

    Create book model

---
+++ src/app/models/book.ts
@@ -0,0 +1,12 @@
+export interface Book {
+  id: number;
+  title: string;
+  author: string;
+  description: string;
+  startDate: string;
+  finishDate: string;
+  status: string;
+  rating: number;
+  image: string;
+  userId: number;
+}
